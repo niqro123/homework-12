56a439acbf84d2c5dfa89ea7121d09033493c83e
uventa <niqacadzegio5@gmail.com>
2022-12-07 18:23:03 +0400

    first commit

---
+++ js/node.js
@@ -0,0 +1,19 @@
+let gageba=() =>{ 
+    let a=document.getElementById('password2').value;
+let c=a;
+console.log(c)
+
+if(c==123){console.log('პაროლი სწორია');document.getElementById('pasuxi').innerHTML='პაროლი სწორია'}
+
+else{console.log('პაროლი არ არის სწორი')
+
+document.getElementById('pasuxi').innerHTML='პაროლი არ არის სწორი'
+
+
+
+
+}
+
+
+
+}
